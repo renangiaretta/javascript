15c44f8422fa02d45f16a8158ed4d083ace59514
renangiaretta <110490586+renangiaretta@users.noreply.github.com>
2022-08-09 17:28:15 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let nome = "Renan"
+let sobreNome = "Giareta"
+let anoNascimento = 1992
+let idade = 2022 - anoNascimento
+let cidade = "Medianeira - PR."
+let nomeCompleto = `${nome} ${sobreNome}`
+let quem = "Eu sou o"
+let tem = "tenho"
+let moro = "e moro em"
+let age = "anos"
+let frase = `${quem} ${nome} ${sobreNome} ${tem} ${idade}  ${age} ${moro} ${cidade}`
+console.log(frase)
